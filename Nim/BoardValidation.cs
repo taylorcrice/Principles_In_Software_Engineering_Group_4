fbced5dee24931de554d4817a6e7d69d11c41b86
Ryan Adams <radams@student.neumont.edu>
2013-04-28 18:05:25 -0600

    moved the unique move list into boardValidation

--- Nim/BoardValidation.cs
+++ Nim/BoardValidation.cs
@@ -7,8 +7,24 @@ namespace Nim
 {
     class BoardValidation
     {
+        public List<TurnData> uniqueMoves { get; set; }
 
-        
+        public BoardValidation(Row[] rows)
+        {
+            uniqueMoves = new List<TurnData>();
+            for (int row1 = 0; row1 <= rows[0].maxRowSize; row1++)
+            {
+                for (int row2 = 0; row2 <= rows[1].maxRowSize; row2++)
+                {
+                    for (int row3 = 0; row3 <= rows[2].maxRowSize; row3++)
+                    {
+                        int[] board = { row1, row2, row3 };
+                        uniqueMoves.Add(new TurnData(board, 0));
+                    }
+                }
+            }
+        }
+
         public bool gameoverCheck(int[] boardState)
         {
             return (boardState[0] + boardState[1] + boardState[3]) <= 0;
@@ -26,9 +42,9 @@ namespace Nim
         }
 
 
-        public static List<TurnData> getValidBoardState()
+        public List<TurnData> getValidBoardState(int[] boardState)
         {
-            int[] currentBoardState = GameBoard.getBoardState();
+            int[] currentBoardState = boardState;
             List<TurnData> validMoveData = new List<TurnData>();
             for (int i = 0; i < ComputerData.uniqueMove.Count; i++)
             {
